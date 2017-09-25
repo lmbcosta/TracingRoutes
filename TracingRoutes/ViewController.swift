953fa3fbbcad92b575ff14cbb5daaa923b6c8cc4
lmbcosta <lmbcosta@hotmail.com>
2017-09-26 00:40:44 +0100

    searchbar added

--- TracingRoutes/ViewController.swift
+++ TracingRoutes/ViewController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var directionsLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var currentCoordinates: CLLocationCoordinate2D!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        searchBar.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getDirections(at destination: MKMapItem) {
+        // Get source direction
+        let sourcePlaceMark = MKPlacemark(coordinate: currentCoordinates)
+        let sourceMapItem = MKMapItem(placemark: sourcePlaceMark)
+        
+        // Prepare directions with directon Request
+        let directionsRequest = MKDirectionsRequest()
+        directionsRequest.source = sourceMapItem
+        directionsRequest.destination = destination
+        directionsRequest.transportType = .automobile
+        
+        // Directions
+        let directions = MKDirections(request: directionsRequest)
+        // TODO: - Calulate directions
+        
     }
-
-
 }
+
+// Extensions
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        guard let location = manager.location else {return}
+        currentCoordinates = location.coordinate
+        // Zoom into user's location
+        mapView.userTrackingMode = .followWithHeading
+    }
+}
+
+// MARK: - Search Bar Extensions
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Close keyboard
+        searchBar.endEditing(true)
+        // Init a local search request
+        let localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        localSearchRequest.region = MKCoordinateRegion(center: currentCoordinates, span: span)
+        
+        // Create an actual localSearch
+        let localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start(completionHandler: {response, error in
+            guard let response = response else {
+                print(error.debugDescription)
+                return
+            }
+            
+            guard error == nil else {
+                print(error.debugDescription)
+                return
+            }
+            
+            guard let mapItem = response.mapItems.first else {
+                return
+            }
+            
+            // MapItem
+            print(response.mapItems)
+            self.getDirections(at: mapItem)
+        })
+    }
+}
+
+
+
 
